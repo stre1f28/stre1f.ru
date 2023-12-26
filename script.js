ddb732aca68da31e330f60be3632b28a5a976f10
Aleksandr I <stre1f2803@gmail.com>
2023-12-26 16:38:59 +0800

    Change some things in script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-function replaceSpanWithLink() {
+function HideBarAndShowContent() {
     const articleElement = document.getElementById('content')
     const cursorElement = document.querySelector('.typed-cursor')
     const barElement = document.getElementById('bar')
     articleElement.style.width = '31ch';
+    // Workaround, i need to fix this in future
+    articleElement.style.margin = '0 0 0 1ch';
     cursorElement.style.display = 'inline';
     barElement.style.display = 'none';
-    // Workaround i need to fix this in future
-    articleElement.style.margin = '0 0 0 1ch';
 }
 
-setTimeout(replaceSpanWithLink, 1800);
+setTimeout(HideBarAndShowContent, 1800);
 
 // some code for typed.js library
 let typed = new Typed('#typed', {
